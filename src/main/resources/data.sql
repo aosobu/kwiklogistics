95a477493b4ef9fb2a7973101135b968125dc87d
13401547 <a13401547@mac13401547L.local>
2022-11-09 20:16:14 +0100

    refactor: improves drone loading algorithm to perform priority based loading using age of medication order in database. This is to ensure order is loaded on a first-in-first-out basis

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
---data for medication table
-insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight)
-values  ('2022-11-08 04:28:37.328478', '2022-11-08 04:28:37.328478',  'aug-100', 'images/Twitter-logo-png-1.png', 'augmentin-1009', 100);
-insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight)
-values  ('2022-11-08 05:28:37.328478', '2022-11-08 05:28:37.328478',  'combat-200', 'images/Twitter-logo-png-1.png', 'combatrin-200g', 200);
-insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight)
-values  ('2022-11-08 06:28:37.328478', '2022-11-08 06:28:37.328478',  'loratidine-300', 'images/Twitter-logo-png-1.png', 'loratidine-300g', 300);
-insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight)
-values  ('2022-11-08 07:28:37.328478', '2022-11-08 07:28:37.328478',  'benylin-400', 'images/Twitter-logo-png-1.png', 'benylin-400g', 400);
+data for medication table
+insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight, value)
+values  ('2022-11-08 04:28:37.328478', '2022-11-08 04:28:37.328478',  'aug-100', 'images/Twitter-logo-png-1.png', 'augmentin-1009', 100, 100);
+insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight, value)
+values  ('2022-11-08 05:28:37.328478', '2022-11-08 05:28:37.328478',  'combat-200', 'images/Twitter-logo-png-1.png', 'combatrin-200g', 200. 200);
+insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight, value)
+values  ('2022-11-08 06:28:37.328478', '2022-11-08 06:28:37.328478',  'loratidine-300', 'images/Twitter-logo-png-1.png', 'loratidine-300g', 300. 300);
+insert into medication_tb (created_date, last_modified_date, code, image_path, name, weight, value)
+values  ('2022-11-08 07:28:37.328478', '2022-11-08 07:28:37.328478',  'benylin-400', 'images/Twitter-logo-png-1.png', 'benylin-400g', 400. 400);
 
 --data for medication order
 insert into medication_order_tb (medication_id, state, created_date, last_modified_date, drone_id, version)
@@ -27,8 +27,8 @@ values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 45, 'MIDDLEWEI
 insert into drone_tb (created_date, last_modified_date, battery_capacity, model, serial_number, state, weight_limit)
 values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 23, 'LIGHTWEIGHT', 'XVH5547674736UTPO', 'IDLE', 400);
 insert into drone_tb (created_date, last_modified_date, battery_capacity, model, serial_number, state, weight_limit)
-values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 21, 'HEAVYWEIGHT', 'XVH5547674737UTPO', 'IDLE', 300);
+values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 76, 'HEAVYWEIGHT', 'XVH5547674737UTPO', 'IDLE', 300);
 insert into drone_tb (created_date, last_modified_date, battery_capacity, model, serial_number, state, weight_limit)
-values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 21, 'HEAVYWEIGHT', 'XVH5547674738UTPO', 'IDLE', 500);
+values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 87, 'HEAVYWEIGHT', 'XVH5547674738UTPO', 'IDLE', 500);
 insert into drone_tb (created_date, last_modified_date, battery_capacity, model, serial_number, state, weight_limit)
 values ('2022-11-08 18:00:16.69169', '2022-11-08 18:00:16.69169', 21, 'HEAVYWEIGHT', 'XVH5547674739UTPO', 'IDLE', 500);
---
+++ src/test/java/com/example/houseofprayerlogistics/data/TestData.java
@@ -0,0 +1,2 @@
+package com.example.houseofprayerlogistics.data;public class TestData {
+}
